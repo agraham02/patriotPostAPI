cb7b953dc49f4ca47f3541e040b62d3bafccb177
agraham02 <acgraham02@gmail.com>
2022-06-27 17:12:58 -0400

    change

--- routes/logIn.js
+++ routes/logIn.js
@@ -4,20 +4,23 @@ const logInRouter = express.Router();
 const logInController = require("../controllers/logInController");
 
 logInRouter.get("/login", (req, res) => {
-    res.send("Login Page");
+  res.send("Login Page");
 });
 
-logInRouter.post("/login", passport.authenticate("local"));
+logInRouter.post("/login", passport.authenticate("local"), (req, res) => {
+  console.log("Getting profile...");
+  res.redirect("/profile");
+});
 
 logInRouter.post("/register", logInController.register);
 
 logInRouter.post("/logout", (req, res, next) => {
-    req.logOut((error) => {
-        if (error) {
-            return next(error);
-        }
-        res.redirect("/");
-    });
+  req.logOut((error) => {
+    if (error) {
+      return next(error);
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = logInRouter;
