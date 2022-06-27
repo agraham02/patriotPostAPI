53a103e7e8c035f033095c1b852c4d694917d46f
agraham02 <acgraham02@gmail.com>
2022-06-26 22:50:45 -0400

    ssl config

--- models/database.js
+++ models/database.js
@@ -2,16 +2,21 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const devConfig = {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 };
 
 const productionConfig = {
-    connectionString: process.env.DATABASE_URL
-}
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
-const pool = new Pool(process.env.NODE_ENV === "production" ? productionConfig : devConfig);
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? productionConfig : devConfig
+);
 module.exports = pool;
