1624289a157b37ad70692bb6b0f977380c4ccaa8
agraham02 <acgraham02@gmail.com>
2022-06-28 13:40:09 -0400

    pre implementation of jwt

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get("*", (req, res) => {
     res.send("That path does not exists");
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 })
