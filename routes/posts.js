a10832b16bf8957880fbd0b679d27012644b7e0e
agraham02 <acgraham02@gmail.com>
2022-06-28 17:46:35 -0400

    new

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const passport = require("passport");
 const postsRouter = express.Router();
 const postController = require("../controllers/postsController");
+
+postsRouter.use(passport.authenticate("jwt", { session: false }));
 
 postsRouter.get("/", postController.getPosts);
 
