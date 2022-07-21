324de59f0f1f2f27ac9b8500116f5cb2ef363a72
agraham02 <acgraham02@gmail.com>
2022-07-20 23:52:45 -0400

    query fix

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -213,7 +213,7 @@ const unlikeComment = async (req, res, next) => {
 
 const refreshPostLikesAndCommentsCnt = async (req, res, next) => {
     try {
-        const {user} = await req.body;
+        const user = await req.user;
         const { postId } = req.params;
         const likeCnt = await queries.posts.likes.getPostsLikesCnt(postId);
         const commentCnt = await queries.posts.comments.getPostsCommentCnt(
